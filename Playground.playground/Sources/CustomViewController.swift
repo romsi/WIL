dc5c0f616ba20c9cc007663d5b5623f7fd85e0bd
Romain ASNAR <romain.asnar@gmail.com>
2019-03-11 19:02:18 +0100

    feat(storyboard): Create a playground file that uses storyboards.
    A custom view controller from a storyboard is displayed using PlaygroundPage live view.

---
+++ Playground.playground/Sources/CustomViewController.swift
@@ -0,0 +1,5 @@
+import UIKit
+/*:
+ Defining `CustomViewController` in the source folder will include it to the module that will be used in the storyboard.
+ */
+public class CustomViewController: UIViewController { }
