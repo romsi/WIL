c8210162895f493ddd110cdd9eb76122ea652108
Romain ASNAR <romain.asnar@gmail.com>
2019-07-31 09:46:53 +0200

    docs(algorithm): Add algorithm playground file with documentation.

---
+++ Algorithm.playground/Pages/Table Of Content.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+ # Algorithm & Data Structure
+ 
+ In computer science, an algorithm is a set of instructions, typically to solve a class of problems or perform a computation. Algorithms are unambiguous specifications for performing calculation, data processing, automated reasoning, and other tasks.
+ 
+ The goal is to describe how algorithms work. The focus is on clarity and readability of the code, not on making a reusable library that you can drop into projects.
+ 
+ Knowing a little bit about the strategies used by algorithms to solve tricky problems gives ideas for improvements that can be done in your own code.
+ 
+ Knowing more data structures than just the standard array and dictionary gives you a bigger collection of tools to build apps.
+ */
