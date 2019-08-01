2d27de6a459dcfba268d1111897428b93b3304f4
Romain ASNAR <romain.asnar@gmail.com>
2019-08-01 09:21:55 +0200

    feat(quicksort): Implement a basic quicksort.

--- Algorithm.playground/Pages/Quicksort.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/Quicksort.xcplaygroundpage/Contents.swift
@@ -15,3 +15,15 @@
  
  The pivot selection and partitioning steps can be done in several different ways; the choice of specific implementation schemes greatly affects the algorithm's performance.
  */
+func quicksort<T: Comparable>(_ a: [T]) -> [T] {
+    guard a.count > 1 else { return a }
+    
+    let pivot = a[a.count/2]
+    let less = a.filter { $0 < pivot }
+    let equal = a.filter { $0 == pivot }
+    let greater = a.filter { $0 > pivot }
+
+    return quicksort(less) + equal + quicksort(greater)
+}
+let list = [ 10, 0, 3, 9, 2, 14, 8, 27, 1, 5, 8, -1, 26 ]
+quicksort(list)
